3981266a629bf09e367a5e4ff022232aa55469cd
Nsikan Akpan <nsikan.akpan@betmakers.com>
2023-11-28 01:51:48 -0500

    added v1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JFrame frame= new JFrame();
+        frame.setTitle("Pizza Project");
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
