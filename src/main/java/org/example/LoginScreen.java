dcc06ead6759f7bcd6b20678c6c97b44e82dcfec
Nsikan Akpan <nsikan.akpan@betmakers.com>
2023-11-28 02:34:36 -0500

    login

--- src/main/java/org/example/LoginScreen.java
+++ src/main/java/org/example/LoginScreen.java
@@ -2,6 +2,8 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LoginScreen extends JFrame {
 
@@ -22,6 +24,58 @@ public class LoginScreen extends JFrame {
             @Override
             public Dimension getPreferredSize() {
                 return new Dimension();
+            }
+        });
+
+        setContentPane(new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+            }
+
+            @Override
+            public Dimension getPreferredSize() {
+                return new Dimension();
+            }
+        });
+
+        JTextField usernameField = new JTextField(20);
+        JTextField passwordField = new JTextField(20);
+        JButton loginButton = new JButton("Login");
+
+        JPanel inputPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        inputPanel.add(new JLabel("Username:"), gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        inputPanel.add(usernameField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        inputPanel.add(new JLabel("Password:"), gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        inputPanel.add(passwordField, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        inputPanel.add(loginButton, gbc);
+
+        getContentPane().add(inputPanel, BorderLayout.CENTER);
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = passwordField.getText();
+                System.out.println("Username: " + username + ", Password: " + password);
             }
         });
 
